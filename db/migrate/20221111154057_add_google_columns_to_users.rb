0fbef62b42682399324a7e1baefa41225320e667
Pudding(Lloyd) <95976269+Kiyo-18@users.noreply.github.com>
2023-01-31 15:47:17 +0800

    Delete db/migrate directory

--- db/migrate/20221111154057_add_google_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddGoogleColumnsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :full_name, :string
-    add_column :users, :uid, :string
-    add_column :users, :avatar_url, :string
-    add_column :users, :provider, :string
-  end
-end
